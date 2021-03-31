3ef86ec1cfb714bb137756106538a6d8a53dd631
Sillis <ville.siikaluoma@gmail.com>
2021-03-31 21:40:48 +0300

    Make a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Render dummy text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/supermarket/i);
+  expect(textElement).toBeInTheDocument();
 });
